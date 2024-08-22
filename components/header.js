cafc11b82947f1b6da30691396cb1b4e8c912403
AngelSiete <andraco660@gmail.com>
2024-08-22 11:45:57 +0200

    changing links to client-rendering

--- components/header.js
+++ components/header.js
@@ -1,19 +1,21 @@
 import Link from "next/link";
 import logoImg from '@/assets/logo.png'
-import classes from './header.module.css'
 import Image from "next/image";
+import NavLink from "./nav-link";
+import classes from './header.module.css'
 
 export default function Header(){
+    
     return(
         <header className={classes.header}>
             <Link className={classes.logo} href="/"><Image src={logoImg}priority></Image>FoodOrder-NEXT_JS</Link>
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href="/meals">Browse Meals</Link>
+                        <NavLink href="/meals">Browse all Meals </NavLink>
                     </li>
                     <li>
-                        <Link href="/community">Browse the Community</Link>
+                        <NavLink href="/community">Browse our Communities </NavLink>
                     </li>
                 </ul>
             </nav>
