70b3d27e41046ac0b57cb7f879550197e0df35ff
AngelSiete <andraco660@gmail.com>
2024-08-22 12:12:07 +0200

    added meal page to connect to db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,18 @@
+import Link from 'next/link'
+import classes from './page.module.css'
+
 export default function MealsPage(){
     return(
         <>
-        <h1>All Meals Page</h1>
-        
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created <span className={classes.highlight}>by you</span>
+            </h1>
+            <p className={classes.cta}>
+                <Link href="/meals/share">Share your recipe</Link>
+            </p>
+        </header>
+        <main className={classes.main}></main>
         </>
     )
 }
